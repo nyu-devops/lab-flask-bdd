53762a9ffdd20b1314dd55a9cb0809e2fdd42831
John Rofrano <johnnyroy@johnrofrano.com>
2017-12-08 14:36:22 -0500

    changed expectation

--- features/steps/pet_steps.py
+++ features/steps/pet_steps.py
@@ -12,7 +12,7 @@ from compare import expect, ensure
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 
 from app import server
 
@@ -72,8 +72,15 @@ def step_impl(context, button):
 
 @then(u'I should see "{name}" in the results')
 def step_impl(context, name):
-    element = context.driver.find_element_by_id('search_results')
-    expect(element.text).to_contain(name)
+    #element = context.driver.find_element_by_id('search_results')
+    #expect(element.text).to_contain(name)
+    found = WebDriverWait(context.driver, 10).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    expect(found).to_be(True)
 
 @then(u'I should not see "{name}" in the results')
 def step_impl(context, name):
@@ -83,8 +90,15 @@ def step_impl(context, name):
 
 @then(u'I should see the message "{message}"')
 def step_impl(context, message):
-    element = context.driver.find_element_by_id('flash_message')
-    expect(element.text).to_contain(message)
+    #element = context.driver.find_element_by_id('flash_message')
+    #expect(element.text).to_contain(message)
+    found = WebDriverWait(context.driver, 10).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    expect(found).to_be(True)
 
 ##################################################################
 # This code works because of the following naming convention:
@@ -96,11 +110,15 @@ def step_impl(context, message):
 @then(u'I should see "{text_string}" in the "{element_name}" field')
 def step_impl(context, text_string, element_name):
     element_id = 'pet_' + element_name.lower()
-    element = WebDriverWait(context.driver, 10).until(
-        EC.text_to_be_present_in_element_value((By.ID, element_id), text_string)
+    #element = context.driver.find_element_by_id(element_id)
+    found = WebDriverWait(context.driver, 10).until(
+        expected_conditions.text_to_be_present_in_element_value(
+            (By.ID, element_id),
+            text_string
+        )
     )
-    element = context.driver.find_element_by_id(element_id)
-    expect(element.get_attribute('value')).to_equal(text_string)
+    #expect(element.get_attribute('value')).to_equal(text_string)
+    expect(found).to_be(True)
 
 @when(u'I change "{element_name}" to "{text_string}"')
 def step_impl(context, element_name, text_string):
