524cfe737e1daf8d7ae42467ecf2e97676238e28
John Rofrano <johnnyroy@johnrofrano.com>
2019-04-25 07:06:41 -0400

    added wait on seeing message

--- features/steps/pet_steps.py
+++ features/steps/pet_steps.py
@@ -111,15 +111,15 @@ def step_impl(context, name):
 
 @then('I should see the message "{message}"')
 def step_impl(context, message):
-    element = context.driver.find_element_by_id('flash_message')
-    expect(element.text).to_contain(message)
-    # found = WebDriverWait(context.driver, WAIT_SECONDS).until(
-    #     expected_conditions.text_to_be_present_in_element(
-    #         (By.ID, 'flash_message'),
-    #         message
-    #     )
-    # )
-    # expect(found).to_be(True)
+    # element = context.driver.find_element_by_id('flash_message')
+    # expect(element.text).to_contain(message)
+    found = WebDriverWait(context.driver, WAIT_SECONDS).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    expect(found).to_be(True)
 
 ##################################################################
 # This code works because of the following naming convention:
