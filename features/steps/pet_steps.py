daa735a90308daf3f9780bbb0f279f63581af790
John Rofrano <johnnyroy@johnrofrano.com>
2019-04-26 06:54:16 -0400

    changed select dropdown check to visible text

--- features/steps/pet_steps.py
+++ features/steps/pet_steps.py
@@ -56,11 +56,11 @@ def step_impl(context, element_name, text_string):
     element.clear()
     element.send_keys(text_string)
 
-@when('I select "{value}" in the "{element_name}" dropdown')
-def step_impl(context, value, element_name):
+@when('I select "{text}" in the "{element_name}" dropdown')
+def step_impl(context, text, element_name):
     element_id = 'pet_' + element_name.lower()
     element = Select(context.driver.find_element_by_id(element_id))
-    element.select_by_value(value)
+    element.select_by_visible_text(text)
 
 @then('the "{element_name}" field should be empty')
 def step_impl(context, element_name):
