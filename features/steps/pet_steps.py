f2c34526cbd547f30c853f5483fbb155380df384
John Rofrano <johnnyroy@johnrofrano.com>
2019-04-26 15:20:39 -0400

    changed .travis.yml to use couchdb

--- features/steps/pet_steps.py
+++ features/steps/pet_steps.py
@@ -2,6 +2,9 @@
 Pet Steps
 
 Steps file for Pet.feature
+
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
 """
 from os import getenv
 import logging
