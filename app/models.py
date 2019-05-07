d0e8171c1b891482507abad7da1191094b509147
John Rofrano <johnnyroy@johnrofrano.com>
2019-05-06 22:04:49 -0400

    added sleep to avoid Cloudant throttle

--- app/models.py
+++ app/models.py
@@ -33,6 +33,7 @@ Docker Note:
 
 import os
 import json
+import time
 import logging
 from retry import retry
 from cloudant.client import Cloudant
@@ -49,6 +50,8 @@ CLOUDANT_PASSWORD = os.environ.get('CLOUDANT_PASSWORD', 'pass')
 RETRY_COUNT = int(os.environ.get('RETRY_COUNT', 10))
 RETRY_DELAY = int(os.environ.get('RETRY_DELAY', 1))
 RETRY_BACKOFF = int(os.environ.get('RETRY_BACKOFF', 2))
+
+SLEEP_TIME = float(os.environ.get('SLEEP_TIME', 0.5))
 
 class DataValidationError(Exception):
     """ Custom Exception with data validation fails """
@@ -96,7 +99,9 @@ class Pet(object):
         except KeyError:
             document = None
         if document:
+            time.sleep(SLEEP_TIME) # avoid Cloudant throttle
             document.update(self.serialize())
+            time.sleep(SLEEP_TIME) # avoid Cloudant throttle
             document.save()
 
 
@@ -121,6 +126,7 @@ class Pet(object):
         except KeyError:
             document = None
         if document:
+            time.sleep(SLEEP_TIME) # avoid Cloudant throttle]
             document.delete()
 
 
@@ -185,6 +191,7 @@ class Pet(object):
     def remove_all(cls):
         """ Removes all documents from the database (use for testing)  """
         for document in cls.database:
+            time.sleep(SLEEP_TIME) # avoid Cloudant throttle
             document.delete()
 
     @classmethod
@@ -194,6 +201,7 @@ class Pet(object):
         """ Query that returns all Pets """
         results = []
         for doc in cls.database:
+            time.sleep(SLEEP_TIME) # avoid Cloudant throttle
             pet = Pet().deserialize(doc)
             pet.id = doc['_id']
             results.append(pet)
@@ -211,6 +219,7 @@ class Pet(object):
         query = Query(cls.database, selector=kwargs)
         results = []
         for doc in query.result:
+            time.sleep(SLEEP_TIME) # avoid Cloudant throttle
             pet = Pet()
             pet.deserialize(doc)
             results.append(pet)
