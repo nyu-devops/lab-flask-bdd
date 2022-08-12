b122950ce15cabd2e732ab58c899167e8ef69951
John Rofrano <johnnyroy@johnrofrano.com>
2022-08-12 01:53:38 +0000

    Removed pool size for sqlite support

--- service/config.py
+++ service/config.py
@@ -13,7 +13,7 @@ DATABASE_URI = os.getenv(
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_POOL_SIZE = 2
+# SQLALCHEMY_POOL_SIZE = 2
 
 # Secret for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "sup3r-s3cr3t")
