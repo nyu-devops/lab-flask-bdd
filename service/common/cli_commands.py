bb7f0a4ca2c99002d6447fd8a8e363d49204528b
John Rofrano <johnnyroy@johnrofrano.com>
2022-11-07 01:12:15 +0000

    Renamed create-db to db-create

--- service/common/cli_commands.py
+++ service/common/cli_commands.py
@@ -7,10 +7,10 @@ from service.models import db
 
 ######################################################################
 # Command to force tables to be rebuilt
-# Usage: flask create-db
+# Usage: flask db-create
 ######################################################################
-@app.cli.command("create-db")
-def create_db():
+@app.cli.command("db-create")
+def db_create():
     """
     Recreates a local database. You probably should not use this on
     production.
