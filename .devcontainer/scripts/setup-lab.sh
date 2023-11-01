5257f9859a7f09c588f731529639c361728e14c9
John Rofrano <johnnyroy@johnrofrano.com>
2023-11-01 19:01:08 +0000

    Moved setup to post container creation

---
+++ .devcontainer/scripts/setup-lab.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Setup the lab environment after container is started using:
+#   "postCreateCommand": "bash /app/.devcontainer/scripts/setup-lab.sh"
+# 
+echo "Setting up Flask BDD lab environment..."
+
+# Create .env file if it doesn't exist
+if [ ! -f .env ]; then 
+    cp dot-env-example .env
+fi
+
+# Pull container images for the lab
+docker pull python:3.11-slim
+
+# Modify /etc/hosts to map the local container registry
+echo Setting up cluster-registry...
+sudo bash -c "echo '127.0.0.1    cluster-registry' >> /etc/hosts"
+
+echo "Setup complete"
