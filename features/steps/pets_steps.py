b0db62b904a3dea539ac94f1ec05dec93ce58f49
John Rofrano <johnnyroy@johnrofrano.com>
2024-04-16 15:26:46 +0000

    Added timeouts to requests calls

--- features/steps/pets_steps.py
+++ features/steps/pets_steps.py
@@ -1,5 +1,5 @@
 ######################################################################
-# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2024 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,24 +23,29 @@ For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
 import requests
-from behave import given
+from compare3 import expect
+from behave import given  # pylint: disable=no-name-in-module
 
 # HTTP Return Codes
 HTTP_200_OK = 200
 HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
 
+WAIT_TIMEOUT = 60
+
+
 @given('the following pets')
 def step_impl(context):
     """ Delete all Pets and load new ones """
 
-    # List all of the pets and delete them one by one
+    # Get a list all of the pets
     rest_endpoint = f"{context.base_url}/pets"
-    context.resp = requests.get(rest_endpoint)
-    assert(context.resp.status_code == HTTP_200_OK)
+    context.resp = requests.get(rest_endpoint, timeout=WAIT_TIMEOUT)
+    expect(context.resp.status_code).equal_to(HTTP_200_OK)
+    # and delete them one by one
     for pet in context.resp.json():
-        context.resp = requests.delete(f"{rest_endpoint}/{pet['id']}")
-        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
+        context.resp = requests.delete(f"{rest_endpoint}/{pet['id']}", timeout=WAIT_TIMEOUT)
+        expect(context.resp.status_code).equal_to(HTTP_204_NO_CONTENT)
 
     # load the database with new pets
     for row in context.table:
@@ -51,5 +56,5 @@ def step_impl(context):
             "gender": row['gender'],
             "birthday": row['birthday']
         }
-        context.resp = requests.post(rest_endpoint, json=payload)
-        assert(context.resp.status_code == HTTP_201_CREATED)
+        context.resp = requests.post(rest_endpoint, json=payload, timeout=WAIT_TIMEOUT)
+        expect(context.resp.status_code).equal_to(HTTP_201_CREATED)
