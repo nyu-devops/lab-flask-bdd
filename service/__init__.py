413fee936be1a8c2b413b1ad8623d74041374a77
litou-lyh <1614895393@qq.com>
2024-08-14 03:22:50 +0000

    project preparation: convert to restx, remove k8s

--- service/__init__.py
+++ service/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2016, 2021 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2019 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,16 +12,26 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# spell: ignore Rofrano restx gunicorn
 """
-Package: service
-Package for the application models and service routes
-This module creates and configures the Flask app and sets up the logging
-and SQL database
+Microservice module
+
+This module contains the microservice code for
+    service
+    models
 """
 import sys
 from flask import Flask
-from service import config
+from flask_restx import Api
 from service.common import log_handlers
+from service import config
+
+# NOTE: Do not change the order of this code
+# The Flask app must be created
+# BEFORE you import modules that depend on it !!!
+
+# Will be initialize when app is created
+api = None  # pylint: disable=invalid-name
 
 
 ############################################################
@@ -29,24 +39,40 @@ from service.common import log_handlers
 ############################################################
 def create_app():
     """Initialize the core application."""
-    # Create Flask application
+
+    # Create the Flask app
     app = Flask(__name__)
     app.config.from_object(config)
 
-    # Initialize Plugins
-    # pylint: disable=import-outside-toplevel
-    from service.models import db
-    db.init_app(app)
+    # Turn off strict slashes because it violates best practices
+    app.url_map.strict_slashes = False
+
+    ######################################################################
+    # Configure Swagger before initializing it
+    ######################################################################
+    global api
+    api = Api(
+        app,
+        version="1.0.0",
+        title="Pet Demo REST API Service",
+        description="This is a sample server Pet store server.",
+        default="pets",
+        default_label="Pet shop operations",
+        doc="/apidocs",  # default also could use doc='/apidocs/'
+        prefix="/api",
+    )
 
     with app.app_context():
-        # Dependencies require we import the routes AFTER the Flask app is created
-        # pylint: disable=wrong-import-position, wrong-import-order, unused-import
-        from service import routes, models  # noqa: F401 E402
-        from service.common import error_handlers, cli_commands  # noqa: F401, E402
+        # Import the routes After the Flask app is created
+        # pylint: disable=import-outside-toplevel
+        from service import routes, models  # noqa: F401, E402
+        from service.common import error_handlers   # pylint: disable=unused-import
 
         try:
-            # models.init_db(app)  # make our sqlalchemy tables
-            db.create_all()
+            # Initialize Plugins
+            # pylint: disable=import-outside-toplevel
+            models.db.init_app(app)
+            
         except Exception as error:  # pylint: disable=broad-except
             app.logger.critical("%s: Cannot continue", error)
             # gunicorn requires exit code 4 to stop spawning workers when they die
@@ -56,7 +82,7 @@ def create_app():
         log_handlers.init_logging(app, "gunicorn.error")
 
         app.logger.info(70 * "*")
-        app.logger.info("  P E T   S T O R E   S E R V I C E  ".center(70, "*"))
+        app.logger.info("  P E T   S E R V I C E   R U N N I N G  ".center(70, "*"))
         app.logger.info(70 * "*")
 
         app.logger.info("Service initialized!")
--- service/routes.py
+++ service/routes.py
@@ -1,5 +1,5 @@
 ######################################################################
-# Copyright 2016, 2024 John J. Rofrano. All Rights Reserved.
+# Copyright 2016, 2022 John J. Rofrano. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,221 +16,279 @@
 
 # spell: ignore Rofrano jsonify restx dbname
 """
-Pet Store Service with UI
+Pet Store Service with Swagger
+
+Paths:
+------
+GET / - Displays a UI for Selenium testing
+GET /pets - Returns a list all of the Pets
+GET /pets/{id} - Returns the Pet with a given id number
+POST /pets - creates a new Pet record in the database
+PUT /pets/{id} - updates a Pet record in the database
+DELETE /pets/{id} - deletes a Pet record in the database
 """
-from flask import jsonify, request, url_for, abort
+
+import secrets
+from functools import wraps
+from flask import request
 from flask import current_app as app  # Import Flask application
+from flask_restx import Resource, fields, reqparse, inputs
 from service.models import Pet, Gender
 from service.common import status  # HTTP Status Codes
+from . import api
 
 
 ######################################################################
-# GET HEALTH CHECK
-######################################################################
-@app.route("/health")
-def health_check():
-    """Let them know our heart is still beating"""
-    return jsonify(status=200, message="Healthy"), status.HTTP_200_OK
-
-
-######################################################################
-# GET INDEX
+# Configure the Root route before OpenAPI
 ######################################################################
 @app.route("/")
 def index():
-    """Base URL for our service"""
+    """Index page"""
     return app.send_static_file("index.html")
 
 
+# Define the model so that the docs reflect what can be sent
+create_model = api.model(
+    "Pet",
+    {
+        "name": fields.String(required=True, description="The name of the Pet"),
+        "category": fields.String(
+            required=True,
+            description="The category of Pet (e.g., dog, cat, fish, etc.)",
+        ),
+        "available": fields.Boolean(
+            required=True, description="Is the Pet available for purchase?"
+        ),
+        # pylint: disable=protected-access
+        "gender": fields.String(
+            enum=Gender._member_names_, description="The gender of the Pet"
+        ),
+        "birthday": fields.Date(required=True, description="The day the pet was born"),
+    },
+)
+
+pet_model = api.inherit(
+    "PetModel",
+    create_model,
+    {
+        "_id": fields.String(
+            readOnly=True, description="The unique id assigned internally by service"
+        ),
+    },
+)
+
+# query string arguments
+pet_args = reqparse.RequestParser()
+pet_args.add_argument(
+    "name", type=str, location="args", required=False, help="List Pets by name"
+)
+pet_args.add_argument(
+    "category", type=str, location="args", required=False, help="List Pets by category"
+)
+pet_args.add_argument(
+    "available",
+    type=inputs.boolean,
+    location="args",
+    required=False,
+    help="List Pets by availability",
+)
+
+
 ######################################################################
-# LIST ALL PETS
+#  PATH: /pets/{id}
 ######################################################################
-@app.route("/pets", methods=["GET"])
-def list_pets():
-    """Returns all of the Pets"""
-    app.logger.info("Request to list Pets...")
-
-    pets = []
-
-    # Parse any arguments from the query string
-    category = request.args.get("category")
-    name = request.args.get("name")
-    available = request.args.get("available")
-    gender = request.args.get("gender")
-
-    if category:
-        app.logger.info("Find by category: %s", category)
-        pets = Pet.find_by_category(category)
-    elif name:
-        app.logger.info("Find by name: %s", name)
-        pets = Pet.find_by_name(name)
-    elif available:
-        app.logger.info("Find by available: %s", available)
-        # create bool from string
-        available_value = available.lower() in ["true", "yes", "1"]
-        pets = Pet.find_by_availability(available_value)
-    elif gender:
-        app.logger.info("Find by gender: %s", gender)
-        # create enum from string
-        gender_value = getattr(Gender, gender.upper())
-        pets = Pet.find_by_gender(gender_value)
-    else:
-        app.logger.info("Find all")
-        pets = Pet.all()
-
-    results = [pet.serialize() for pet in pets]
-    app.logger.info("[%s] Pets returned", len(results))
-    return jsonify(results), status.HTTP_200_OK
-
-
-######################################################################
-# READ A PET
-######################################################################
-@app.route("/pets/<int:pet_id>", methods=["GET"])
-def get_pets(pet_id):
+@api.route("/pets/<pet_id>")
+@api.param("pet_id", "The Pet identifier")
+class PetResource(Resource):
     """
-    Retrieve a single Pet
+    PetResource class
 
-    This endpoint will return a Pet based on it's id
+    Allows the manipulation of a single Pet
+    GET /pet{id} - Returns a Pet with the id
+    PUT /pet{id} - Update a Pet with the id
+    DELETE /pet{id} -  Deletes a Pet with the id
     """
-    app.logger.info("Request to Retrieve a pet with id [%s]", pet_id)
 
-    # Attempt to find the Pet and abort if not found
-    pet = Pet.find(pet_id)
-    if not pet:
-        abort(status.HTTP_404_NOT_FOUND, f"Pet with id '{pet_id}' was not found.")
+    # ------------------------------------------------------------------
+    # RETRIEVE A PET
+    # ------------------------------------------------------------------
+    @api.doc("get_pets")
+    @api.response(404, "Pet not found")
+    @api.marshal_with(pet_model)
+    def get(self, pet_id):
+        """
+        Retrieve a single Pet
 
-    app.logger.info("Returning pet: %s", pet.name)
-    return jsonify(pet.serialize()), status.HTTP_200_OK
+        This endpoint will return a Pet based on it's id
+        """
+        app.logger.info("Request to Retrieve a pet with id [%s]", pet_id)
+        pet = Pet.find(pet_id)
+        if not pet:
+            abort(status.HTTP_404_NOT_FOUND, f"Pet with id '{pet_id}' was not found.")
+        return pet.serialize(), status.HTTP_200_OK
 
+    # ------------------------------------------------------------------
+    # UPDATE AN EXISTING PET
+    # ------------------------------------------------------------------
+    @api.doc("update_pets")
+    @api.response(404, "Pet not found")
+    @api.response(400, "The posted Pet data was not valid")
+    @api.expect(pet_model)
+    @api.marshal_with(pet_model)
+    def put(self, pet_id):
+        """
+        Update a Pet
 
-######################################################################
-# CREATE A NEW PET
-######################################################################
-@app.route("/pets", methods=["POST"])
-def create_pets():
-    """
-    Create a Pet
-    This endpoint will create a Pet based the data in the body that is posted
-    """
-    app.logger.info("Request to Create a Pet...")
-    check_content_type("application/json")
+        This endpoint will update a Pet based the body that is posted
+        """
+        app.logger.info("Request to Update a pet with id [%s]", pet_id)
+        pet = Pet.find(pet_id)
+        if not pet:
+            abort(status.HTTP_404_NOT_FOUND, f"Pet with id '{pet_id}' was not found.")
+        app.logger.debug("Payload = %s", api.payload)
+        data = api.payload
+        pet.deserialize(data)
+        pet.id = pet_id
+        pet.update()
+        return pet.serialize(), status.HTTP_200_OK
 
-    pet = Pet()
-    # Get the data from the request and deserialize it
-    data = request.get_json()
-    app.logger.info("Processing: %s", data)
-    pet.deserialize(data)
+    # ------------------------------------------------------------------
+    # DELETE A PET
+    # ------------------------------------------------------------------
+    @api.doc("delete_pets")
+    @api.response(204, "Pet deleted")
+    def delete(self, pet_id):
+        """
+        Delete a Pet
 
-    # Save the new Pet to the database
-    pet.create()
-    app.logger.info("Pet with new id [%s] saved!", pet.id)
+        This endpoint will delete a Pet based the id specified in the path
+        """
+        app.logger.info("Request to Delete a pet with id [%s]", pet_id)
+        pet = Pet.find(pet_id)
+        if pet:
+            pet.delete()
+            app.logger.info("Pet with id [%s] was deleted", pet_id)
 
-    # Return the location of the new Pet
-    location_url = url_for("get_pets", pet_id=pet.id, _external=True)
-    return jsonify(pet.serialize()), status.HTTP_201_CREATED, {"Location": location_url}
-
-
-######################################################################
-# UPDATE AN EXISTING PET
-######################################################################
-@app.route("/pets/<int:pet_id>", methods=["PUT"])
-def update_pets(pet_id):
-    """
-    Update a Pet
-
-    This endpoint will update a Pet based the body that is posted
-    """
-    app.logger.info("Request to Update a pet with id [%s]", pet_id)
-    check_content_type("application/json")
-
-    # Attempt to find the Pet and abort if not found
-    pet = Pet.find(pet_id)
-    if not pet:
-        abort(status.HTTP_404_NOT_FOUND, f"Pet with id '{pet_id}' was not found.")
-
-    # Update the Pet with the new data
-    data = request.get_json()
-    app.logger.info("Processing: %s", data)
-    pet.deserialize(data)
-
-    # Save the updates to the database
-    pet.update()
-
-    app.logger.info("Pet with ID: %d updated.", pet.id)
-    return jsonify(pet.serialize()), status.HTTP_200_OK
+        return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
-# DELETE A PET
+#  PATH: /pets
 ######################################################################
-@app.route("/pets/<int:pet_id>", methods=["DELETE"])
-def delete_pets(pet_id):
-    """
-    Delete a Pet
+@api.route("/pets", strict_slashes=False)
+class PetCollection(Resource):
+    """Handles all interactions with collections of Pets"""
 
-    This endpoint will delete a Pet based the id specified in the path
-    """
-    app.logger.info("Request to Delete a pet with id [%s]", pet_id)
+    # ------------------------------------------------------------------
+    # LIST ALL PETS
+    # ------------------------------------------------------------------
+    @api.doc("list_pets")
+    @api.expect(pet_args, validate=True)
+    @api.marshal_list_with(pet_model)
+    def get(self):
+        """Returns all of the Pets"""
+        app.logger.info("Request to list Pets...")
+        pets = []
+        args = pet_args.parse_args()
+        if args["category"]:
+            app.logger.info("Filtering by category: %s", args["category"])
+            pets = Pet.find_by_category(args["category"])
+        elif args["name"]:
+            app.logger.info("Filtering by name: %s", args["name"])
+            pets = Pet.find_by_name(args["name"])
+        elif args["available"] is not None:
+            app.logger.info("Filtering by availability: %s", args["available"])
+            pets = Pet.find_by_availability(args["available"])
+        else:
+            app.logger.info("Returning unfiltered list.")
+            pets = Pet.all()
 
-    # Delete the Pet if it exists
-    pet = Pet.find(pet_id)
-    if pet:
-        app.logger.info("Pet with ID: %d found.", pet.id)
-        pet.delete()
+        app.logger.info("[%s] Pets returned", len(pets))
+        results = [pet.serialize() for pet in pets]
+        return results, status.HTTP_200_OK
 
-    app.logger.info("Pet with ID: %d delete complete.", pet_id)
-    return {}, status.HTTP_204_NO_CONTENT
+    # ------------------------------------------------------------------
+    # ADD A NEW PET
+    # ------------------------------------------------------------------
+    @api.doc("create_pets")
+    @api.response(400, "The posted data was not valid")
+    @api.expect(create_model)
+    @api.marshal_with(pet_model, code=201)
+    def post(self):
+        """
+        Creates a Pet
+        This endpoint will create a Pet based the data in the body that is posted
+        """
+        app.logger.info("Request to Create a Pet")
+        pet = Pet()
+        app.logger.debug("Payload = %s", api.payload)
+        pet.deserialize(api.payload)
+        pet.create()
+        app.logger.info("Pet with new id [%s] created!", pet.id)
+        location_url = api.url_for(PetResource, pet_id=pet.id, _external=True)
+        return pet.serialize(), status.HTTP_201_CREATED, {"Location": location_url}
+
+    # ------------------------------------------------------------------
+    # DELETE ALL PETS (for testing only)
+    # ------------------------------------------------------------------
+    @api.doc("delete_all_pets")
+    @api.response(204, "All Pets deleted")
+    def delete(self):
+        """
+        Delete all Pet
+
+        This endpoint will delete all Pet only if the system is under test
+        """
+        app.logger.info("Request to Delete all pets...")
+        if "TESTING" in app.config and app.config["TESTING"]:
+            Pet.remove_all()
+            app.logger.info("Removed all Pets from the database")
+        else:
+            app.logger.warning("Request to clear database while system not under test")
+
+        return "", status.HTTP_204_NO_CONTENT
 
 
 ######################################################################
-# PURCHASE A PET
+#  PATH: /pets/{id}/purchase
 ######################################################################
-@app.route("/pets/<int:pet_id>/purchase", methods=["PUT"])
-def purchase_pets(pet_id):
-    """Purchasing a Pet makes it unavailable"""
-    app.logger.info("Request to purchase pet with id: %d", pet_id)
+@api.route("/pets/<pet_id>/purchase")
+@api.param("pet_id", "The Pet identifier")
+class PurchaseResource(Resource):
+    """Purchase actions on a Pet"""
 
-    # Attempt to find the Pet and abort if not found
-    pet = Pet.find(pet_id)
-    if not pet:
-        abort(status.HTTP_404_NOT_FOUND, f"Pet with id '{pet_id}' was not found.")
+    @api.doc("purchase_pets")
+    @api.response(404, "Pet not found")
+    @api.response(409, "The Pet is not available for purchase")
+    def put(self, pet_id):
+        """
+        Purchase a Pet
 
-    # you can only purchase pets that are available
-    if not pet.available:
-        abort(
-            status.HTTP_409_CONFLICT,
-            f"Pet with id '{pet_id}' is not available.",
-        )
-
-    # At this point you would execute code to purchase the pet
-    # For the moment, we will just set them to unavailable
-
-    pet.available = False
-    pet.update()
-
-    app.logger.info("Pet with ID: %d has been purchased.", pet_id)
-    return pet.serialize(), status.HTTP_200_OK
+        This endpoint will purchase a Pet and make it unavailable
+        """
+        app.logger.info("Request to Purchase a Pet")
+        pet = Pet.find(pet_id)
+        if not pet:
+            abort(status.HTTP_404_NOT_FOUND, f"Pet with id [{pet_id}] was not found.")
+        if not pet.available:
+            abort(status.HTTP_409_CONFLICT, f"Pet with id [{pet_id}] is not available.")
+        pet.available = False
+        pet.update()
+        app.logger.info("Pet with id [%s] has been purchased!", pet.id)
+        return pet.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
 
-def check_content_type(content_type) -> None:
-    """Checks that the media type is correct"""
-    if "Content-Type" not in request.headers:
-        app.logger.error("No Content-Type specified.")
-        abort(
-            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            f"Content-Type must be {content_type}",
-        )
 
-    if request.headers["Content-Type"] == content_type:
-        return
+def abort(error_code: int, message: str):
+    """Logs errors before aborting"""
+    app.logger.error(message)
+    api.abort(error_code, message)
 
-    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
-    abort(
-        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-        f"Content-Type must be {content_type}",
-    )
+
+def data_reset():
+    """Removes all Pets from the database"""
+    Pet.remove_all()
